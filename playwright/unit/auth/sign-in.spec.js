c7119615b54d5f5a7a511dc1451b7454ce3108f4
lpereiras <lpereira.qa@gmail.com>
2025-09-19 19:05:04 -0300

    SRTB-01(fix)

--- playwright/unit/auth/sign-in.spec.js
+++ playwright/unit/auth/sign-in.spec.js
@@ -3,12 +3,13 @@ import { LOGIN_ROUTE, REGISTER_ROUTE } from '../../support/apiRoutes'
 import { testUser } from '../../support/factories/testUser';
 import { Type } from '../../support/factories/auth';
 
-test.beforeAll(async ({request}) => {
-    await request.post(REGISTER_ROUTE, {
-            data: testUser
-    })
-});
 test.describe('POST /auth/login', () => {
+    test.beforeAll(async ({request}) => {
+        await request.post(REGISTER_ROUTE, {
+                data: testUser
+        })
+    });
+
     test('', async ({request}) => {
         const response = await request.post(LOGIN_ROUTE, {
             data: {
@@ -19,6 +20,6 @@ test.describe('POST /auth/login', () => {
         const responseBody = await response.json();
 
         expect(response.status()).toBe(200);
-        // expect(responseBody.user).toHaveProperty(Type.ID)
+        expect(responseBody.data.user).toHaveProperty(Type.ID)
     });
 });
